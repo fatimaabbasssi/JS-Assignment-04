18d84c2a0906b032c982349db8b4601103555f84
fatimaabbasssi <artistfatimaabbasi@gmail.com>
2024-11-14 22:52:07 +0500

    43-to-52 chapters

---
+++ script.js
@@ -0,0 +1,188 @@
+// document.write("<h2> JS-Assignment-PDF-LastChapters </h2>")
+
+
+
+//chapter============43to48===========
+
+//===============Question-01==================
+
+// function clickme (){
+//     alert("BOO!")
+// }
+
+
+
+
+//===============Question-02==================
+
+// function image(){
+//     alert("Thanks for purchase")
+// }
+
+
+
+//===============Question-03==================
+
+// var row1 = document.getElementById("row");
+
+// function dele(){
+//   row1.children[0].innerHTML = ""
+//   row1.children[1].innerHTML = ""
+//   row1.children[2].innerHTML = ""
+//   row1.children[3].innerHTML = ""
+// }
+
+// var row2 = document.getElementById("row-1");
+
+// function dele1(){
+//   row2.children[0].innerHTML = ""
+//   row2.children[1].innerHTML = ""
+//   row2.children[2].innerHTML = ""
+//   row2.children[3].innerHTML = ""
+// }
+
+// var row3 = document.getElementById("row-2");
+
+// function dele2(){
+//   row3.children[0].innerHTML = ""
+//   row3.children[1].innerHTML = ""
+//   row3.children[2].innerHTML = ""
+//   row3.children[3].innerHTML = ""
+// }
+
+// var row4 = document.getElementById("row-3");
+
+// function dele3(){
+//   row4.children[0].innerHTML = ""
+//   row4.children[1].innerHTML = ""
+//   row4.children[2].innerHTML = ""
+//   row4.children[3].innerHTML = ""
+// }
+
+// var row5 = document.getElementById("row-4");
+
+// function dele4(){
+//   row5.children[0].innerHTML = ""
+//   row5.children[1].innerHTML = ""
+//   row5.children[2].innerHTML = ""
+//   row5.children[3].innerHTML = ""
+// }
+
+// var row6 = document.getElementById("row-5");
+
+// function dele5(){
+//   row6.children[0].innerHTML = ""
+//   row6.children[1].innerHTML = ""
+//   row6.children[2].innerHTML = ""
+//   row6.children[3].innerHTML = ""
+// }
+
+// var row7 = document.getElementById("row-6");
+
+// function dele6(){
+//   row7.children[0].innerHTML = ""
+//   row7.children[1].innerHTML = ""
+//   row7.children[2].innerHTML = ""
+//   row7.children[3].innerHTML = ""
+// }
+
+// var row8 = document.getElementById("row-7");
+
+// function dele7(){
+//   row8.children[0].innerHTML = ""
+//   row8.children[1].innerHTML = ""
+//   row8.children[2].innerHTML = ""
+//   row8.children[3].innerHTML = ""
+// }
+
+// var row9 = document.getElementById("row-8");
+
+// function dele8(){
+//   row9.children[0].innerHTML = ""
+//   row9.children[1].innerHTML = ""
+//   row9.children[2].innerHTML = ""
+//   row9.children[3].innerHTML = ""
+// }
+
+// var row0 = document.getElementById("row-9");
+
+// function dele9(){
+//   row0.children[0].innerHTML = ""
+//   row0.children[1].innerHTML = ""
+//   row0.children[2].innerHTML = ""
+//   row0.children[3].innerHTML = ""
+// }
+
+
+//===============Question-04==================
+
+
+//DONE
+
+
+//===============Question-05==================
+
+
+// var con = 0;
+// function count(button){
+// con++
+//     document.getElementById("dis").innerHTML = con
+// }
+
+
+
+
+//=============================================END========================================================
+
+
+
+
+//chapter=========================49to52==============================
+
+//===============Question-01================== -->
+
+// var namie = document.getElementById("idname")
+// var email = document.getElementById("idemail")
+// var pwrd = document.getElementById("idpass")
+
+// function sub(event){
+
+//     event.preventDefault();
+
+//     namie.innerHTML = "NAME : " +  document.getElementById("namee").value
+//     email.innerHTML = "EMAIL : " + document.getElementById("email").value
+//     pwrd.innerHTML = "PASSWORD : " + document.getElementById("pass").value
+// }
+
+//===============Question-02==================
+
+// var clickme = document.getElementById("linkme")
+
+// function readme1(){
+
+//     clickme.innerText = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat sit facere at quas minus vero maiores esse voluptates, 
+// ipsum dolor sit amet consectetur adipisicing elit. Repellat sit facere at quas minus vero maiores esse voluptates, 
+// orem ipsum dolor sit amet consectetur adipisicing elit. Repellat sit facere at quas minus vero maiores esse voluptates, 
+// impedit praesentium consectetur aut modi vitae doloremque repellendus culpa? Omnis harum facilis laudantium 
+// repudiandae officiis inventore ipsum modi possimus odit, corrupti expedita ea consectetur illum sequi 
+// libero sapiente beatae, reprehenderit doloribus pariatur.`
+
+// }
+
+
+
+ //===============Question-03================== -->
+ 
+
+ //var namie = document.getElementById("idname")
+// var email = document.getElementById("idemail")
+// var pwrd = document.getElementById("idpass")
+
+// function sub(event){
+
+//     event.preventDefault();
+
+//     namie.innerHTML = "NAME : " +  document.getElementById("namee").value
+//     email.innerHTML = "EMAIL : " + document.getElementById("email").value
+//     pwrd.innerHTML = "PASSWORD : " + document.getElementById("pass").value
+// }
